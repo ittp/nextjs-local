e15339dda79c04a3c2afb58dd68b4aacdd260547
ittp <61617660+ittp@users.noreply.github.com>
2023-02-03 05:40:17 +0300

    4 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from 'next/head';
-// import styles from '../style//s/Home.module.css';
 import { Card } from 'antd';
 
 import theme from '../theme';
+import axios from 'axios';
 
+axios.get('', {
+  headers: {
+    Authorization:
+      'Bearer 41b5e76cb1fd4b76ced9421a8f3174e381094182e66b877d1a95b337c6acc661',
+  },
+});
 export default function Home() {
   return (
     <div className={'container'}>
@@ -12,7 +18,9 @@ export default function Home() {
       </Head>
 
       <main className={'data'}>
-        <Card title={1}>Card</Card>
+        <Card title={1} description={'Data'} extra={'Extra'} avatar={'Avatar'}>
+          Card
+        </Card>
         <Card title={1}>Card</Card>
         <Card title={1}>Card</Card>
       </main>
