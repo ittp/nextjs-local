aa93e078dd6e1dcfdb145e7ac8a6fee3545ddd24
ittp <61617660+ittp@users.noreply.github.com>
2023-02-03 05:25:10 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+// import styles from '../style//s/Home.module.css';
 import { Card } from 'antd';
 
 import theme from '../theme';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={'container'}>
       <Head>
         <title>App</title>
       </Head>
 
-      <main className={styles.main}>
+      <main className={'data'}>
         <Card title={1}>Card</Card>
         <Card title={1}>Card</Card>
         <Card title={1}>Card</Card>
