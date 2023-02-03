b78b3fd00c94596b08242061a104e1aecca6ca3c
ittp <61617660+ittp@users.noreply.github.com>
2023-02-03 04:48:50 +0300

    4 files modified

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -10,22 +10,26 @@ let request = {
 };
 
 let url = 'http://api.vapezone.pro:28080';
+const { ZabbixClient } = require('zabbix-client');
 
-let zabbix = axios.create({
-  baseURL: url + '/api_jsonrpc.php',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json-rpc',
-  },
-});
+// No calls are made here yet
+const client = new ZabbixClient(url + '/api_jsonrpc.php');
+// let zabbix = axios.create({
+//   baseURL: url + '/api_jsonrpc.php',
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json-rpc',
+//   },
+// });
 
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   // debugger;
 
-  let zr = zabbix
-    .post({ url: '', data: request })
-    .then((response) => response.data);
+  // A token will be fetched and saved for further use
+  const api = await client.login('Admin', 'tyui1234');
+
+  console.log(api);
 
   res.status(200).json({ name: 'Hello, world!' });
 };
--- pages/api/zabbix/info.js
+++ pages/api/zabbix/info.js
@@ -1,21 +1,21 @@
-let request = {
-  jsonrpc: '2.0',
-  method: 'apiinfo.version',
-  id: 1,
-  auth: null,
-  params: {},
-};
+// let request = {
+//   jsonrpc: '2.0',
+//   method: 'apiinfo.version',
+//   id: 1,
+//   auth: null,
+//   params: {},
+// };
 
-const axios = require('axios');
+// const axios = require('axios');
 
-let url = 'http://api.vapezone.pro:28080';
+// let url = 'http://api.vapezone.pro:28080';
 
-let zabbix = axios.create({
-  baseURL: url + '/api_jsonrpc.php',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json-rpc',
-  },
-});
+// let zabbix = axios.create({
+//   baseURL: url + '/api_jsonrpc.php',
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json-rpc',
+//   },
+// });
 
-zabbix.post({ url: '', data: request });
+// zabbix.post({ url: '', data: request });
