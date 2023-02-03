01706f29f0e847d10040cb34113360eb0e8d82ef
ittp <61617660+ittp@users.noreply.github.com>
2023-02-03 04:37:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-local
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-goiuq2)
---
+++ theme.js
@@ -0,0 +1,9 @@
+let theme = {
+  token: {
+    borderRadius: 15,
+    colorPrimary: '#FA541C',
+    wireframe: true,
+  },
+};
+
+export default theme;
